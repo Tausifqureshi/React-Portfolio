c5298778fecf5c3f58f809533ba17618f45c0b2c
Tausifqureshi <tausifqureshi504@gmail.com>
2025-07-23 09:50:19 +0530

    Updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -90,8 +90,7 @@
 import React from 'react';
 import { Zap, Code, Palette } from 'lucide-react';
 
-function About
-() {
+function About() {
   return (
     <section id="about" className="py-20 bg-white dark:bg-gray-900">
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,3 @@
-
-
-
 import React from 'react';
 import { Github, ExternalLink, Star } from 'lucide-react';
 
