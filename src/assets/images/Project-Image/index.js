42f82b8bcfefc06ab1d826228dbeaa9df60f2695
Tausifqureshi <tausifqureshi504@gmail.com>
2026-07-12 09:10:16 +0530

    Updated now Displayname showcase wiht lighthouse profomance

--- src/assets/images/Project-Image/index.js
+++ src/assets/images/Project-Image/index.js
@@ -1,9 +1,9 @@
 import Storefusion from './StoreFusion.png';
 import Udmy from './Udmy.png';
 import Rodan from './Rodan.png';
+import Taskboard from './taskboard.png';
 
-
-export {Storefusion, Udmy, Rodan}
+export {Storefusion, Udmy, Rodan, Taskboard}
 
 
 
